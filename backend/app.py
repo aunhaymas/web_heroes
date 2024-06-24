f281ee4868de991ca3d10100cdb02840ae97d167
Alfredo Godoy <agodoy@fi.uba.ar>
2024-06-24 15:49:31 -0300

    chore: home redirection to the heroes api

--- backend/app.py
+++ backend/app.py
@@ -8,12 +8,24 @@ from db.characters import *
 app = Flask(__name__)
 CORS(app)
 
+# HOME
+@app.route("/")
+def home():
+    return"""
+<html>
+<body>
+<h1>Welcome to HEROES API</h1>
+<a href="/characters">Go to HomePage</a>
+</body>
+</html>
+    """
 
 # DEVOLVER JSON CON CHARS
 @app.route("/characters")
-def home():
+def all_characters():
     print(get_all_characters)
     return get_all_characters()
+
 
 
 # DEVOLVER CHAR POR ID
@@ -33,7 +45,7 @@ def remove_character_by_id(id):
 def _create_character():
     name = request.json.get("name")
     names = request.json.get("names")
-    aligment = request.json.get("aligment")
+    alignment = request.json.get("alignment")
     gender = request.json.get("gender")
     publisher = request.json.get("publisher")
     race = request.json.get("race")
@@ -42,8 +54,8 @@ def _create_character():
 
     character = {
         "id": id,
-        "alignment": aligment,
         "gender": gender,
+        "alignment": alignment,
         "image": image,
         "name": name,
         "names": names,
